1563500e21083b7805e940df118126f150f9b81e
Iván Montiel cardona <ivan_montiel2013@hotmail.com>
2021-02-05 00:31:33 -0600

    Auto stash before rebase of "origin/main"

--- src/Analisis.R
+++ src/Analisis.R
@@ -11,29 +11,35 @@ install.packages("dplyr")
 library(dplyr)
 
 
-
 # Lectura del dataset con los registros del PIB
-PIB <- read.csv("datapibeng.csv")
-
-# Seleccionar las dos columnas principales y los ultimos 15 años 
-PIB <- PIB[, c(2,47:62)]
+PIB <- read.csv("Dataset_pib.csv")
 
 #tabla de indices de ingreso 04:19
-#formula Índice de Ingreso = log(valor row) – log(100) / log(75.000) – log(100) = 0,70
-Indice <- (log(PIB[, 2:length(PIB)]) - log(100)) / (log(75000) - log(100))
+#formula Índice de Ingreso = log(x) – log(100) / log(75000) – log(100)
+Indice <- (log(PIB[, 3:length(PIB)]) - log(100)) / (log(40000) - log(100))
 Indice_Ingreso <- cbind(PIB[,1], Indice)
-TablaIndiceIngreso<-data.frame(Indice_Ingreso)
-colnames(TablaIndiceIngreso)[1] <- "Country"
+TablaIndiceIngreso <- data.frame(Indice_Ingreso)
+colnames(TablaIndiceIngreso)[1] <- "Country_Name"
 
 
 # Lectura del dataset con los indices de educacion
-TablaIndiceEducacion <- read.csv("Education index.csv")
+TablaIndiceEducacion <- read.csv("Dataset_edu.csv")
 
-TablaIndiceEducacion <- TablaIndiceEducacion[ , c(2,16:31)]
+TablaIndiceEducacion <- TablaIndiceEducacion[ , c(1,3:18)]
 
 # Lectura del dataset con los registros de esperanza de vida
-Vida <- read.csv("esperanzavida.csv")
+Vida <- read.csv("Dataset_vida.csv")
 
-Vida <- Vida[ , c(1, 48:63)]
+Vida <- Vida[ , c(1, 3:18)]
 
 Indice_Salud <- (Vida[, 2:length(Vida)] - 20) / (85 - 20)
+Indice_Salud <- cbind(Vida[,1], Indice_Salud)
+TablaIndiceSalud<-data.frame(Indice_Salud)
+colnames(TablaIndiceSalud)[1] <- "Country_Name"
+
+# Calculo del IDH
+IDH <-  (TablaIndiceIngreso[, 2:length(TablaIndiceIngreso)] / 3) + 
+        (TablaIndiceEducacion[, 2:length(TablaIndiceEducacion)] / 3) +  
+        (TablaIndiceSalud[, 2:length(TablaIndiceSalud)] / 3)
+
+IDH <- cbind(PIB[1:2],IDH)
